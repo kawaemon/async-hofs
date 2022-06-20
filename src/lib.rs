8e312cdb9d73f599218a28b9e9fa1a63291fba49
kawaemon <me@kawaemon.dev>
2022-06-20 22:56:51 +0900

    define prelude module

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,10 @@ pub mod iter;
 pub mod option;
 pub mod result;
 pub mod stream;
+
+pub mod prelude {
+    pub use crate::iter::AsyncMapExt as _;
+    pub use crate::option::AsyncMapExt as _;
+    pub use crate::result::AsyncMapExt as _;
+    pub use crate::stream::AsyncMapExt as _;
+}
